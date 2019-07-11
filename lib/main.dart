524f6c000406af139ce9b79a489f83c030db182b
Karan Singh <karan@singhkaran.com>
2019-07-11 05:51:46 +0530

    TODO 1: Added Background

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,12 @@ class _StoryPageState extends State<StoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        //TODO: Step 1 - Add background.png to this Container as a background image.
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
         constraints: BoxConstraints.expand(),
         child: SafeArea(
